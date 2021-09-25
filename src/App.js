2306c0a2269551a51e91c65a7290131881727baf
Krazeelazy <mm.mightyme@gmail.com>
2021-09-25 12:41:06 -0400

    Implemented userReducer and userActions to app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,33 +9,27 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-    constructor() {
-        super();
-        
-        this.state = {
-            currentUser: null
-        };
-    }
     
     unsubscribeFromAuth = null;
     
     componentDidMount() {
+        const { setCurrentUser } = this.props; // get the setCurrentUser reducer action from the props (added to props in mapDispatchToProps)
+
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
                 
                 userRef.onSnapshot(snapShot => { // onSnapshot is similar to onStateChange, we're subscribing to the user ref and listening for any changes
-                    this.setState({
-                        currentUser: {
-                            id: snapShot.id,
-                            ...snapShot.data() // need .data() to actually get the data from the snapshot object
-                        }
+                    setCurrentUser({
+                        id: snapShot.id,
+                        ...snapShot.data() // need .data() to actually get the data from the snapshot object
                     });                  
                 });
             } else {// if userAuth is null (user signed out)
-                this.setState({ currentUser: userAuth });
+                setCurrentUser(userAuth);
             }
             
         });
@@ -58,4 +53,8 @@ class App extends React.Component {
         }
     }
     
-export default App;
+    const mapDispatchToProps = dispatch => ({
+        setCurrentUser: user => dispatch(setCurrentUser(user))
+    })
+
+export default connect(null, mapDispatchToProps)(App); // 1st argument null because we don't need any state
