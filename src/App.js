ac2676e7b351cab945db99d55f935ad2a3fae06c
Krazeelazy <mm.mightyme@gmail.com>
2021-09-25 19:10:00 -0400

    Added Checkout page along with appropriate route and redirect if press 'go to checkout button'

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
@@ -48,6 +52,7 @@ class App extends React.Component {
             <Switch>
                 <Route exact path='/' component={HomePage} /> 
                 <Route path='/shop' component={ShopPage} /> 
+                <Route exact path='/checkout' component={CheckoutPage} /> 
                 <Route 
                     exact 
                     path='/signin' 
@@ -73,4 +78,4 @@ const mapStateToProps = createStructuredSelector({
         setCurrentUser: user => dispatch(setCurrentUser(user))
     })
 
-export default connect(mapStateToProps, mapDispatchToProps)(App); // 1st argument null because we don't need any state
+export default connect(mapStateToProps, mapDispatchToProps)(App);
