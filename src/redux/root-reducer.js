0113756d4ce2e1c21a23a8ca2e974816d014f66f
Krazeelazy <mm.mightyme@gmail.com>
2021-09-25 21:07:16 -0400

    Added persistence using redux-persist to use the localStorage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // use localStorage to persist the reducer
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key : 'root',
+    storage,
+    whitelist: ['cart'] // array containing the string names of any reducers we want to store. Only cart because firebase handles user.
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
