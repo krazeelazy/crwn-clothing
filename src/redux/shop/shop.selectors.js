b2007fb13df0f3112ae9c79bf2b963fdf10ac326
Krazeelazy <mm.mightyme@gmail.com>
2021-09-26 11:32:45 -0400

    Added memoization to selectCollection selector in shop.selectors

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,3 +1,4 @@
+import memoize from 'lodash.memoize';
 import { createSelector } from "reselect";
 
 const COLLECTION_ID_MAP = {
@@ -15,10 +16,11 @@ export const selectCollections = createSelector(
     (shop) => shop.collections
 );
 
-export const selectCollection = (collectionUrlParam) =>
+export const selectCollection = memoize((collectionUrlParam) =>
     createSelector(
         [selectCollections], 
         (collections) =>
             collections.find((collection) => 
                 collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-    );
+    )
+);
