8a5a2a1b5b22fc8611c907b4e7c0bbe36483f3e4
Krazeelazy <mm.mightyme@gmail.com>
2021-09-27 18:59:23 -0400

    Added selectIsCollectionLoaded shop selector and used in shop component to fix error

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -24,3 +24,8 @@ export const selectIsCollectionFetching = createSelector(
     [selectShop],
     shop => shop.isFetching
 );
+
+export const selectIsCollectionsLoaded = createSelector(
+    [selectShop],
+    shop => !!shop.collections // tell us if the collections obj. is null
+);
