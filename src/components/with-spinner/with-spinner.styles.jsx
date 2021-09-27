565d53e4ed8a6626710c8fded52840d681990a50
Krazeelazy <mm.mightyme@gmail.com>
2021-09-27 10:17:12 -0400

    Added withSpinner higher order component

---
+++ src/components/with-spinner/with-spinner.styles.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const SpinnerOverlay = styled.div`
+    height: 60vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const SpinnerContainer = styled.div`
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    border: 3px solid rgba(195, 195, 195, 0.6);
+    border-radius: 50%;
+    border-top-color: #636767;
+    /* animation: spin 1s ease-in-out infinite;
+    -webkit-animation: spin 1s ease-in-out infinite;
+
+    @keyframes spin {
+        to {
+        -webkit-transform: rotate(360deg);
+        }
+    }
+    
+    @-webkit-keyframes spin {
+        to {
+        -webkit-transform: rotate(360deg);
+        }
+    } */
+
+    /* had to break apart so it would animate in safari */
+    animation-name: spin;
+    animation-duration: .5s;
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+    
+    @keyframes spin{
+        to{
+        transform:rotate(360deg);
+        }
+    }
+`;
