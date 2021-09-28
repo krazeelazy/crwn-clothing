708e1834323fd591b12cc1a099ec6150ec491a1c
Krazeelazy <mm.mightyme@gmail.com>
2021-09-28 15:02:17 -0400

    made shop.sagas functional (fetches collections data from firestore)

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,35 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import {
+    firestore,
+    convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
+
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure
+} from './shop.actions';
 
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-    yield console.log('I am fired');
+    try{
+        yield console.log('I am fired');
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        // call is the effect that involes the convertCollectionsSnapshotMap method
+        // same as doing const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        // but call allows us to yield and defer control to the middleware
+        const collectionsMap = yield call( 
+            convertCollectionsSnapshotToMap,
+            snapshot
+        );
+        // put is the saga effect for creating actions
+        // like dispatch but can be yielded
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
 }
 
 export function* fetchCollectionsStart() {
