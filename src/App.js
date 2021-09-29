042c8633b1af07d5d4b4bba0cf14347596bbdf60
Krazeelazy <mm.mightyme@gmail.com>
2021-09-29 15:56:04 -0400

    update App component to use useSelector and useDispatch instead of connect, mapStateToProps and mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { useSelector, useDispatch } from 'react-redux';
 
 import './App.css';
 
@@ -15,10 +14,13 @@ import Header from './components/header/header.component';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions'
 
-const App = ({ checkUserSession, currentUser }) => {
+const App = () => {
+    const currentUser = useSelector(selectCurrentUser); // replaces mapStateToProps
+    const dispatch = useDispatch();
+
     useEffect(() => {
-        checkUserSession();
-    }, [checkUserSession]);
+        dispatch(checkUserSession());
+    }, [dispatch]);
     
     return (
         <div>
@@ -42,13 +44,5 @@ const App = ({ checkUserSession, currentUser }) => {
         </div>
         );
     }
-    
-const mapStateToProps = createStructuredSelector({ 
-    currentUser: selectCurrentUser,
-});
 
-const mapDispatchToProps = dispatch => ({
-    checkUserSession: () => dispatch(checkUserSession())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
