5303ba871c509b2b52bdf622bf8a438282f2dff6
Krazeelazy <mm.mightyme@gmail.com>
2021-10-03 16:39:32 -0400

    Updated code to be pwa compliant, added serviceworker, https, updated manifest

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
+var enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -12,9 +13,14 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('/service-worker.js', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+    });
 
     app.get('*', function(req, res) { // give the user the index.html file in client/build when they do any get request
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
